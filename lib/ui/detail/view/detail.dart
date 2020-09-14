91d3879f14499c39fe1ec29505126be7c677f503
canberkb8 <canberkbibican@hotmail.com>
2020-09-14 16:43:17 +0300

    detail page created

---
+++ lib/ui/detail/view/detail.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'detail_view.dart';
+
+class Detail extends StatefulWidget {
+  @override
+  DetailView createState() => DetailView();
+}
---
+++ lib/ui/detail/view/detail_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:travel_blog/ui/detail/viewmodel/detail_viewmodel.dart';
+
+class DetailView extends DetailViewModel {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text("data"));
+  }
+}
