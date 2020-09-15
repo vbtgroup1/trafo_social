e5511e39848406371f80523de3b01207be7ff94c
vb <vbacik.10@gmail.com>
2020-09-15 22:07:56 +0300

    fix to conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:travel_blog/info_screen/info_screen1.dart';
-import 'package:travel_blog/post_page/postpage.dart';
-import 'package:travel_blog/ui/profile_page/view/profile.dart';
-import 'package:travel_blog/ProfileScreen.dart';
-import 'package:travel_blog/ui/detail/view/detail.dart';
-import 'package:travel_blog/ui/home/view/home.dart';
 
 import 'ui/splash/splash.dart';
 
--- lib/ui/home/service/home_service.dart
+++ lib/ui/home/service/home_service.dart
@@ -1,7 +1,6 @@
-import 'package:travel_blog/core/base/service/base_service.dart';
-import 'package:travel_blog/core/constants/constants.dart';
-import 'package:travel_blog/ui/home/model/home_model.dart';
-
+import '../../../core/base/service/base_service.dart';
+import '../../../core/constants/constants.dart';
+import '../model/home_model.dart';
 import 'IHome_service.dart';
 
 class HomeService extends IHomeService {
