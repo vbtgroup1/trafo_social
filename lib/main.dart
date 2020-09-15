2773a0d34f55dace4986ee007e30b397cd311010
canberkb8 <canberkbibican@hotmail.com>
2020-09-15 11:47:37 +0300

    splash screen is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:travel_blog/ui/detail/view/detail.dart';
 
+import 'ui/splash/splash.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Detail(),
+      home: Splash(),
     );
   }
 }
---
+++ lib/ui/splash/splash.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:travel_blog/ui/detail/view/detail.dart';
+import 'package:travel_blog/ui/splash/showup.dart';
+
+void main() => runApp(Splash());
+
+class Splash extends StatefulWidget {
+  Splash({Key key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  double get verticalPadding => MediaQuery.of(context).size.height * 0.1;
+  double get animationHeight => MediaQuery.of(context).size.height * 0.4;
+  double get appTitleSize => 40.0;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => Detail()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(vertical: verticalPadding),
+          child: ListView(
+            children: [
+              Lottie.network(
+                'https://assets7.lottiefiles.com/datafiles/AtGF4p7zA8LpP2R/data.json',
+                height: animationHeight,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: verticalPadding),
+                child: ShowUp(
+                  child: Text(
+                    "Travel Blog",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: appTitleSize, fontWeight: FontWeight.bold),
+                  ),
+                  delay: 2000,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
