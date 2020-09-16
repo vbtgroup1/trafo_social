3a5ed63f5622cd88a9dbecdb30f3aa439fa09dc3
tugbayavuz <tugbayavuz@yandex.com>
2020-09-16 21:25:23 +0300

    Login and register page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:travel_blog/ui/profile_edit_page/view/editProfile.dart';
+import 'package:travel_blog/ui/login_page/logip_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: EditProfile(),
+      home: LoginPage(),
       debugShowCheckedModeBanner: false,
       title: 'Material App',
     );
