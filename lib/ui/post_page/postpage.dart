5762a8a160a8fabd1e647e9a41555cde7c305a74
Süleyman SEZER <me.iamcodder@gmail.com>
2020-09-17 15:05:50 +0300

    Add post screen added

--- lib/ui/post_page/postpage.dart
+++ lib/ui/post_page/postpage.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:dotted_border/dotted_border.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:travel_blog/core/constants/constants.dart';
-import 'package:travel_blog/ui/home/view/home.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
 
 class PostPage extends StatefulWidget {
   @override
@@ -11,101 +14,123 @@ class PostPage extends StatefulWidget {
 }
 
 class _PostPageState extends State<PostPage> {
-  // global image değişkenim. imagepicker bunu kullanıyor.
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBarCreatePost(context),
       body: BodyUI(),
-    );
-  }
-
-  AppBar buildAppBarCreatePost(BuildContext context) {
-    return AppBar(
-      backgroundColor: Colors.transparent,
-      elevation: 0,
-      centerTitle: true,
-      leading: IconButton(
-        icon: Icon(Icons.arrow_back),
-        iconSize: AppConstants.APPBAR_ICONSIZE,
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => Home()));
-        },
-        color: AppConstants.appBarIconColor,
-      ),
-      title: Text(
-        'Create a post',
-        style: AppConstants.appTextStyleTitle,
-      ),
-      actions: [
-        IconButton(
-          icon: Icon(Icons.save),
-          onPressed: () {},
-          color: AppConstants.appBarIconColor,
-        )
-      ],
     );
   }
 }
 
-class BodyUI extends StatelessWidget {
+class BodyUI extends StatefulWidget {
+  @override
+  _BodyUIState createState() => _BodyUIState();
+}
+
+class _BodyUIState extends State<BodyUI> {
+  Future<String> resmiGonder() async {
+    StorageReference resimYeri =
+        FirebaseStorage.instance.ref().child(resimYolu);
+
+    StorageUploadTask yuklemeGorevi = resimYeri.putFile(resim);
+
+    var indirmeUrl =
+        await (await yuklemeGorevi.onComplete).ref.getDownloadURL();
+
+    var url = indirmeUrl.toString();
+
+    print("indirme urlsi: " + url);
+
+    return url;
+  }
+
+  /////////////////////////////////
+  File resim;
+  String resimYolu;
+
+  Future resimAl() async {
+    var secilenResim = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      resim = secilenResim;
+      resimYolu = basename(resim.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
         children: [
+          b1(),
           b2(),
           b3(),
           b4(),
           b5(),
           b6(),
           b7(),
-          b8(context),
+          b8(),
         ],
       ),
     );
   }
 
-  Container b4() {
+  Widget yuklemeAlani() {
     return Container(
-      margin: EdgeInsets.only(left: 30.0, right: 30.0, top: 20.0, bottom: 20.0),
-      height: 200.0,
-      width: 330.0,
-      color: Color(0XFFE3FAFC),
-      child: DottedBorder(
-        color: Colors.blueAccent,
-        strokeWidth: 1,
-        child: Container(
-          padding: EdgeInsets.only(left: 60.0, top: 20.0),
-          child: Column(
-            children: [
-              Container(
-                height: 100,
-                width: 200,
-                child: SvgPicture.asset("assets/images/upload1.svg"),
-              ),
-              Icon(
-                Icons.cloud_upload,
-                color: Colors.blueAccent,
-                size: 40.0,
-              ),
-              Text(
-                "Upload a photo",
-                style: GoogleFonts.montserrat(
-                    color: Colors.blueAccent,
-                    fontSize: 13.0,
-                    fontWeight: FontWeight.w400),
-              ),
-            ],
-          ),
-        ),
+      margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0, bottom: 20.0),
+      height: 200,
+      width: 330,
+      child: Image.file(
+        resim,
       ),
     );
   }
 
-  RaisedButton b8(context) {
+  InkWell b4() {
+    return InkWell(
+      onTap: resimAl,
+      child: Container(
+        margin:
+            EdgeInsets.only(left: 30.0, right: 30.0, top: 20.0, bottom: 20.0),
+        height: 200.0,
+        width: 330.0,
+        color: Color(0XFFE3FAFC),
+        child: DottedBorder(
+            color: Colors.blueAccent,
+            strokeWidth: 1,
+            child: resim == null ? b41() : yuklemeAlani()),
+      ),
+    );
+  }
+
+  Container b41() {
+    return Container(
+      padding: EdgeInsets.only(left: 60.0, top: 20.0),
+      child: Column(
+        children: [
+          Container(
+            height: 100,
+            width: 200,
+            child: SvgPicture.asset("assets/images/upload1.svg"),
+          ),
+          Icon(
+            Icons.cloud_upload,
+            color: Colors.blueAccent,
+            size: 40.0,
+          ),
+          Text(
+            "Upload a photo",
+            style: GoogleFonts.montserrat(
+                color: Colors.blueAccent,
+                fontSize: 13.0,
+                fontWeight: FontWeight.w400),
+          ),
+        ],
+      ),
+    );
+  }
+
+  RaisedButton b8() {
     return RaisedButton(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(40.0),
@@ -114,8 +139,7 @@ class BodyUI extends StatelessWidget {
         ),
       ),
       onPressed: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => Home()));
+        resmiGonder();
       },
       color: Color(0xff83a4d4),
       textColor: Colors.white,
@@ -124,7 +148,7 @@ class BodyUI extends StatelessWidget {
         style: TextStyle(fontSize: 13),
       ),
       padding:
-          EdgeInsets.only(left: 100.0, right: 100.0, top: 15.0, bottom: 15.0),
+      EdgeInsets.only(left: 100.0, right: 100.0, top: 15.0, bottom: 15.0),
     );
   }
 
@@ -156,7 +180,7 @@ class BodyUI extends StatelessWidget {
                 filled: true,
                 hintStyle: new TextStyle(color: Colors.grey[800]),
                 hintText:
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula ligula eu ligula placerat dapibus ut lobortis purus. Aliquam erat volutpat.",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula ligula eu ligula placerat dapibus ut lobortis purus. Aliquam erat volutpat.",
                 fillColor: Colors.white70),
           ),
         ),
@@ -280,6 +304,33 @@ class BodyUI extends StatelessWidget {
             style: GoogleFonts.montserrat(
                 color: Colors.black,
                 fontSize: 20.0,
+                fontWeight: FontWeight.w300),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container b1() {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      padding: EdgeInsets.only(top: 40.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(Icons.arrow_back),
+          Text(
+            "Create a Post",
+            style: GoogleFonts.montserrat(
+                color: Colors.black,
+                fontSize: 25.0,
+                fontWeight: FontWeight.w400),
+          ),
+          Text(
+            "Save Drafts",
+            style: GoogleFonts.montserrat(
+                color: Colors.black,
+                fontSize: 15.0,
                 fontWeight: FontWeight.w300),
           ),
         ],
