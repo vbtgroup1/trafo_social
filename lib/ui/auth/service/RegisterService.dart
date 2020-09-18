7d72e334f8867c8d3c20126edd2ecd8c5f139c63
Süleyman SEZER <me.iamcodder@gmail.com>
2020-09-18 05:17:23 +0300

    Register user added to db

---
+++ lib/ui/auth/service/RegisterService.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:travel_blog/ui/auth/service/IRegisterService.dart';
+import 'package:travel_blog/ui/profile_page/model/user_model.dart';
+
+class RegisterService extends IRegisterService {
+  @override
+  Future registerUserSaveData(UserModel model, String kullaniciId) async {
+    await http.put(
+        "https://fb-travel-app.firebaseio.com/userID/$kullaniciId.json",
+        body: json.encode({
+          'userBirth': model.userBirth,
+          'userEmail': model.userEmail,
+          'userGender': model.userGender,
+          'userJob': model.userJob,
+          'userName': model.userName,
+          'userPass': model.userPass,
+          'userProfileImg': model.userProfileImg
+        }));
+  }
+}
